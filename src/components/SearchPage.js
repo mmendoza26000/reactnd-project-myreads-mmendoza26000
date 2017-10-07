d65a23c9f9286201d854e73334d8479443ece53c
Manuel Mendoza <mmendoza26000@gmail.com>
2017-10-07 12:28:15 -0500

    feat: Display error message if search string is unsupported

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -34,35 +34,37 @@ class SearchPage extends Component {
     }
 
     render(){
-        const { searchedBooks } = this.state;
+        const { searchedBooks, errorString } = this.state;
 
         return(
+             
             <div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <form onSubmit={this.handleSubmit}>
                     <input name="searchField" type="text" placeholder="Search by title or author"/>
                 </form>
 
               </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
 
-                { searchedBooks.map( book => <Book key={book.id} book={book} />) }
+            { errorString ? 
+                (
+                    <div style={{paddingTop: '80px'}}>{errorString}</div>
+                )
+                :
+                (<div className="search-books-results">
+                <ol className="books-grid">
 
-              </ol>
-            </div>
-          </div>
+                    { searchedBooks.map( book => <Book key={book.id} book={book} />) }
+
+                </ol>
+                </div>) 
+            }
+
+
+          </div> 
         )
     }
 
